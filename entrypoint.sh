f662c0837e018ba394985b35fee567c3998371c3
JonZeolla <Zeolla@gmail.com>
2023-12-12 17:57:44 -0500

    fix: infer the host user (#15)

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,7 @@ while [[ ${valid_ip_status} -ne 0 ]]; do
     4) prompt="The provided IP was invalid. What does http://icanhazip.com/ipv4 show? " ;;
     # A status of 5 is a private IP
     5) prompt="You provided a private IP; please use your public IP. What does http://icanhazip.com/ipv4 show? " ;;
-    # A status of 6 is a Cloud9 IP address; they chose the wrong one
+    # A status of 6 is a Cloud9 IP address
     6) prompt="You provided a Cloud9 public IP address, please provide your source computer's IP. What does http://icanhazip.com/ipv4 show? " ;;
   esac
 
@@ -52,8 +52,19 @@ while [[ ${valid_ip_status} -ne 0 ]]; do
 done
 
 # Setup ansible prereqs
-export KEY_FILE="${HOME}/.ssh/ansible_key"
-export LOG_FILE="${HOME}/logs/entrypoint.log"
+DEFAULT_USER="$(awk -F: '$3 == 1000 {print $1}' < /host/etc/passwd)"
+if [ -z "${HOST_USER:-}" ]; then
+  HOST_USER="${DEFAULT_USER:-ec2-user}"
+fi
+HOST_HOME_DIR="/host/home/${HOST_USER}"
+KEY_FILE="${HOST_HOME_DIR}/.ssh/ansible_key"
+LOG_DIR="${HOST_HOME_DIR}/logs"
+LOG_FILE="${LOG_DIR}/entrypoint.log"
+mkdir -p "${LOG_DIR}"
+touch "${LOG_FILE}"
+# This is the desired UID/GID on the host
+chown -R 1000:1000 "${LOG_DIR}"
+
 if [[ -s "${KEY_FILE}" ]]; then
   echo "${KEY_FILE} already exists! Skipping SSH key setup..."
   echo "See ${LOG_FILE} for previous configuration details"
@@ -63,14 +74,15 @@ else
   echo "Generated passphrase: ${SSH_PASS:-empty}" >> "${LOG_FILE}"
   ssh-keygen -N "${SSH_PASS}" -C "Ansible key" -f "${KEY_FILE}" | tee -a "${LOG_FILE}"
 
-  cat "${KEY_FILE}.pub" >> "${HOME}/.ssh/authorized_keys"
-  echo "Updated ${HOME}/.ssh/authorized_keys" | tee -a "${LOG_FILE}"
+  AUTHORIZED_KEYS="${HOST_HOME_DIR}/.ssh/authorized_keys"
+  cat "${KEY_FILE}.pub" >> "${AUTHORIZED_KEYS}"
+  echo "Updated ${AUTHORIZED_KEYS}" | tee -a "${LOG_FILE}"
 
-  ssh-keyscan localhost 2>/dev/null >> "${HOME}/.ssh/known_hosts"
-  echo "Updated ${HOME}/.ssh/known_hosts" | tee -a "${LOG_FILE}"
+  KNOWN_HOSTS="${HOST_HOME_DIR}/.ssh/known_hosts"
+  ssh-keyscan localhost 2>/dev/null >> "${KNOWN_HOSTS}"
+  echo "Updated ${KNOWN_HOSTS}" | tee -a "${LOG_FILE}"
 fi
 
-# Don't take ~/.ssh/config into account, since we will change it as a part of the playbook
+# Don't take the host's ~/.ssh/config into account, since we will change it as a part of the playbook
 export ANSIBLE_SSH_ARGS="-F /dev/null"
-DEFAULT_USER="$(getent passwd 1000 | awk -F: '{print $1}')"
-ansible-playbook ${ANSIBLE_CUSTOM_ARGS:-} -e 'ansible_python_interpreter=/usr/bin/python3' --inventory localhost, --user "${HOST_USER:-${DEFAULT_USER:-ec2-user}}" --private-key="${KEY_FILE}" /etc/app/policy-as-code.yml | tee -a "${LOG_FILE}"
+ansible-playbook ${ANSIBLE_CUSTOM_ARGS:-} -e 'ansible_python_interpreter=/usr/bin/python3' --inventory localhost, --user "${HOST_USER}" --private-key="${KEY_FILE}" /etc/app/policy-as-code.yml | tee -a "${LOG_FILE}"
