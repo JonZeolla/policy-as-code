6b0b2f6edae9ed2a353c855d68da0ad808cbf9c1
Jon Zeolla <Zeolla@gmail.com>
2023-12-12 18:31:17 -0500

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,7 +82,6 @@ else
   echo "Updated ${KNOWN_HOSTS}" | tee -a "${LOG_FILE}"
 fi
 
-# Don't take the host's ~/.ssh/config into account, since we will change it as a part of the playbook
-# Also, use a custom location for the known_hosts file based on how we've mounted the host filesystem
-export ANSIBLE_SSH_ARGS="-F /dev/null -o UserKnownHostsFile=${KNOWN_HOSTS}"
+# Use a custom location for the known_hosts file based on how we've mounted the host filesystem
+export ANSIBLE_SSH_ARGS="-o UserKnownHostsFile=${KNOWN_HOSTS}"
 ansible-playbook ${ANSIBLE_CUSTOM_ARGS:-} -e 'ansible_python_interpreter=/usr/bin/python3' --inventory localhost, --user "${HOST_USER}" --private-key="${KEY_FILE}" /etc/app/policy-as-code.yml | tee -a "${LOG_FILE}"
